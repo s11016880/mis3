e25c88fcef32a76d9527c9791b52f8fa9009ca22
s11016880 <b0979798210@gmail.com>
2023-11-21 11:57:12 +0800

    version 0: html5

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
-from datetime import datetime,timezone,timedelta
+from datetime import datetime
 
 import firebase_admin
 from firebase_admin import credentials, firestore
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
+
+import requests
+from bs4 import BeautifulSoup
+
 
 
 app = Flask(__name__)
@@ -22,15 +26,14 @@ def account():
 
 @app.route("/")
 def index():
-    homepage = "<h1>楊承恩Python網頁1114</h1>"
+    homepage = "<h1>楊承恩Python網頁1121</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=楊子青>傳送使用者暱稱</a><br>"
     homepage += "<a href=/about>楊承恩簡介網頁</a><br>"
     homepage += "<a href=/account>網頁表單輸入帳密傳值</a><br>"
     homepage += "<br><a href=/read>人選之人演員名單1</a><br>"
-    homepage += "<br><a href=/books>圖書精選</a><br>"
-    homepage += "<br><a href=/query>圖書查詢</a><br>"
+    homepage += "<a href=/spider>網路爬蟲抓取子青老師課程</a><br>"
 
     return homepage
 
@@ -60,47 +63,25 @@ def read():
     collection_ref = db.collection("人選之人─造浪者")    
     docs = collection_ref.get()    
     for doc in docs:         
-        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
-    return Result
+        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"
 
-@app.route("/books")
-def books():
-    Result = ""
-    db = firestore.client()
-    collection_ref = db.collection("圖書精選")    
-    docs = collection_ref.get()    
-    for doc in docs: 
-        bk = doc.to_dict()        
-        Result += "書名 : <a herf=" + bk["url"] + ">" + bk["title"] + "</a><br>"
-        Result += "作者:"+ bk["author"] + "<br>"
-        Result += str(bk["anniversary"]) + "週年紀念版<br>"
-        Result += "<img src =" + bk["cover"] + "> </image><br><br>"
-    return Result
+@app.route("/spider")
+def spider():
+    url = "https://www1.pu.edu.tw/~tcyang/course.html"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".team-box")
+    info = ""
+    for x in result:
+        info += "<a href=" + x.find("a").get("href") + ">" + x.text + "</a><br>"
+        info += x.find("a").get("href") + "<br><br>"
+    return info
 
-@app.route("/query" , methods=["GET", "POST"])
-def query():
-    Result = ""
-if request.method == "POST":
-keyword = request.form["keyword"]
-result = "您輸入的關鍵字是:" + keyword
-
-Result = ""
-db = firestore.client()
-collection_ref = db.collection("圖書精選")    
-docs = collection_ref.get()    
-for doc in docs: 
-bk = doc.to_dict() 
- if keyword in bk["title"]:       
-Result += "書名 : <a herf=" + bk["url"] + ">" + bk["title"] + "</a><br>"
-Result += "作者:"+ bk["author"] + "<br>"
-Result += str(bk["anniversary"]) + "週年紀念版<br>"
- Result += "<img src =" + bk["cover"] + "> </image><br><br>"
-return Result
-
- else:
-    return render_template("seekbk.html")
-
-
+    
+    
    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
