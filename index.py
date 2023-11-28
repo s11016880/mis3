1b63c3e400b7a2387422991da70c7293fd111cd6
s11016880 <b0979798210@gmail.com>
2023-11-28 11:46:34 +0800

    fsjk

--- index.py
+++ index.py
@@ -34,6 +34,10 @@ def index():
     homepage += "<a href=/account>網頁表單輸入帳密傳值</a><br>"
     homepage += "<br><a href=/read>人選之人演員名單1</a><br>"
     homepage += "<a href=/spider>網路爬蟲抓取子青老師課程</a><br>"
+    homepage += "<br><a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
+    homepage += "<br><a href=/search>讀取開眼電影即將上映影片</a><br>"
+
+
 
     return homepage
 
@@ -77,6 +81,59 @@ def spider():
         info += "<a href=" + x.find("a").get("href") + ">" + x.text + "</a><br>"
         info += x.find("a").get("href") + "<br><br>"
     return info
+
+@app.route("/movie")
+def movie():
+  url = "http://www.atmovies.com.tw/movie/next/"
+  Data = requests.get(url)
+  Data.encoding = "utf-8"
+  sp = BeautifulSoup(Data.text, "html.parser")
+  result=sp.select(".filmListAllX li")
+  lastUpdate = sp.find("div", class_="smaller09").text[5:]
+
+  for item in result:
+    picture = item.find("img").get("src").replace(" ", "")
+    title = item.find("div", class_="filmtitle").text
+    movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+    hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
+    show = item.find("div", class_="runtime").text.replace("上映日期：", "")
+    show = show.replace("片長：", "")
+    show = show.replace("分", "")
+    showDate = show[0:10]
+    showLength = show[13:]
+
+    doc = {
+        "title": title,
+        "picture": picture,
+        "hyperlink": hyperlink,
+        "showDate": showDate,
+        "showLength": showLength,
+        "lastUpdate": lastUpdate
+      }
+
+    db = firestore.client()
+    doc_ref = db.collection("電影").document(movie_id)
+    doc_ref.set(doc)
+@app.route("/searchQ", methods=["POST","GET"])
+def searchQ():
+    if request.method == "POST":
+        MovieTitle = request.form["MovieTitle"]
+        info = ""
+        db = firestore.client()     
+        collection_ref = db.collection("電影")
+        docs = collection_ref.order_by("showDate").get()
+        for doc in docs:
+            if MovieTitle in doc.to_dict()["title"]: 
+                info += "片名：" + doc.to_dict()["title"] + "<br>" 
+                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
+                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
+        return info
+    else:  
+        return render_template("input.html")
+
+    
+
 
     
     
--- spider.py
+++ spider.py
@@ -1,7 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www1.pu.edu.tw/~tcyang/course.html"
+url = "http://www.atmovies.com.tw/movie/next/"
 Data = requests.get(url)
 Data.encoding = "utf-8"
 
@@ -10,9 +15,31 @@ Data.encoding = "utf-8"
 
 
 sp = BeautifulSoup(Data.text, "html.parser")
-result=sp.select(".team-box")
+result=sp.select(".filmListAllX li")
+lastUpdate = sp.find("div", class_="smaller09").text[5:]
+for item in result:
+  picture = item.find("img").get("src").replace(" ", "")
+  title = item.find("div", class_="filmtitle").text
+  movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+  hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
+  show = item.find("div", class_="runtime").text.replace("上映日期：", "")
+  show = show.replace("片長：", "")
+  show = show.replace("分", "")
+  showDate = show[0:10]
+  showLength = show[13:]
+doc = {
+      "title": title,
+      "picture": picture,
+      "hyperlink": hyperlink,
+      "showDate": showDate,
+      "showLength": showLength,
+      "lastUpdate": lastUpdate
+  }
 
-for x in result:
-	print(x.text)
-	print(x.find("a").get("href"))
-	
+db = firestore.client()
+doc_ref = db.collection("電影").document(movie_id)
+doc_ref.set(doc)
+
+
+
+		
